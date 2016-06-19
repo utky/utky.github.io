085b1d8ba724a44c56e72a533175c19f9fae82b4
utky <ilyaletre@gmail.com>
2016-06-19 12:30:51 +0900

    fix link loss to font-awsome

--- src/site.hs
+++ src/site.hs
@@ -19,6 +19,10 @@ main = hakyllWith config $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "fonts/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
